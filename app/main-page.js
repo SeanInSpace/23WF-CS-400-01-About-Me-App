d5b8afe53f8e391ba67395571c75a09c41c63712
SeanInSpace <seanmbarnes9@gmail.com>
2023-10-18 12:40:25 -0500

    Navigation

--- app/main-page.js
+++ app/main-page.js
@@ -4,3 +4,15 @@ export function onNavigatingTo(args) {
   const page = args.object;
   page.bindingContext = createViewModel();
 }
+export function navigateToBio(args) {
+    const page = args.object.page;
+    page.frame.navigate("bio-page");
+}
+export function navigateToSkills(args) {
+  const page = args.object.page;
+  page.frame.navigate("skills-page");
+}
+export function navigateToContact(args) {
+  const page = args.object.page;
+  page.frame.navigate("contact-page");
+}
